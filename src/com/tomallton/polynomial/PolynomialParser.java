76cb44ff6cd0265c9175e9d9ca25713e5b37a08c
Zylem <tomallton1@gmail.com>
2018-06-30 13:48:07 +0100

    Fixes

--- src/com/tomallton/polynomial/PolynomialParser.java
+++ src/com/tomallton/polynomial/PolynomialParser.java
@@ -1,22 +1,24 @@
 package com.tomallton.polynomial;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 import java.util.StringJoiner;
 
 public class PolynomialParser {
-	
+
 	public static void main(String[] args) {
-		System.out.println(infixToReversePolish("33+21"));
+		System.out.println(infixToReversePolish("1+2*C-D"));
 	}
 
 	public static String infixToReversePolish(String infix) {
 
+		infix = infix.replaceAll(" ", "");
+
 		StringJoiner reversePolish = new StringJoiner(" ");
 
-		Stack<String> stack = new Stack<>();
-		StringBuilder number = new StringBuilder();
-
-		infix = infix.replaceAll(" ", "");
+		Stack<Character> operatorStack = new Stack<>();
+		Queue<Character> numberQueue = new LinkedList<>();
 
 		char[] chars = infix.toCharArray();
 
@@ -24,31 +26,55 @@ public class PolynomialParser {
 			char c = chars[i];
 
 			switch (c) {
-			case 0:
-			case 1:
-			case 2:
-			case 3:
-			case 4:
-			case 5:
-			case 6:
-			case 7:
-			case 8:
-			case 9:
-				number.append(c);
+			case '0':
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+				numberQueue.add(c);
 				break;
+			default:
+
+				StringBuilder number = new StringBuilder();
+
+				while (!numberQueue.isEmpty()) {
+					number.append(numberQueue.poll());
+				}
+
+				reversePolish.add(number.toString());
+			}
+
+			switch (c) {
 			case '+':
 			case '-':
 			case '*':
 			case '/':
-				reversePolish.add(number.toString());
-				
-				
-				stack.push(number.toString());
-				number = new StringBuilder();
-
-				reversePolish.add(stack.pop());
+			case '^':
+				if (!operatorStack.isEmpty())
+				System.out.println(operatorStack.peek() + " " + c + " " + (operatorStack.peek() <= c));
+				while (!operatorStack.isEmpty() && operatorStack.peek() <= c) {
+					reversePolish.add(new String(new char[] { operatorStack.pop() }));
+				}
+				operatorStack.push(c);
 			}
 
+		}
+
+		StringBuilder number = new StringBuilder();
+
+		while (!numberQueue.isEmpty()) {
+			number.append(numberQueue.poll());
+		}
+
+		reversePolish.add(number.toString());
+
+		while (!operatorStack.isEmpty()) {
+			reversePolish.add(new String(new char[] { operatorStack.pop() }));
 		}
 
 		return reversePolish.toString();
